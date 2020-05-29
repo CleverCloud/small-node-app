eb72911ec5e8a9ff93c1accaba8616a961ba75d8
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-05-29 22:55:41 +0200

    v2.0.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import http from 'http';
 
 const port = 8080;
-const server = http.createServer((req, res) => res.end(`Hello World!`));
+const server = http.createServer((req, res) => res.end(`Hello World!!! This is awesome`));
 server.listen(port);
 console.log(`HTTP server running at port ${port}`);
